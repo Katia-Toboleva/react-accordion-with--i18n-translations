e446f91bae4527b4ad3197c9996fd3f8a8a43e52
Katia-2020 <kateflights@gmail.com>
2020-12-23 20:53:33 +0000

    modified icon component

--- client/app/components/icon/icon.jsx
+++ client/app/components/icon/icon.jsx
@@ -1,32 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
 import * as icons from './svg';
 import styles from './icon.scss';
 
 const cx = classnames.bind(styles);
 
-const Icon = (props) => {
-  const {
-    icon, theme,
-  } = props;
+const Icon = ({ icon, theme, size, isOpened }) => (
+  <div
+    className={cx('icon', {
+      [`icon--theme-${theme}`]: theme,
+      [`icon--size-${size}`]: size,
+      'icon--is-opened': isOpened,
+    })}
+  >
+    <i dangerouslySetInnerHTML={{ __html: icons[icon] }} />
+  </div>
+);
 
-  const glyth = icons[icon];
-
-  return (
-    <i
-      className={cx('icon', {
-        [`icon--theme-${theme}`]: theme,
-      })}
-      dangerouslySetInnerHTML={{ __html: glyth }}
-    />
-  );
-};
-
-
-Icon.propTypes = {
-  icon: PropTypes.string,
-  theme: PropTypes.string,
-};
 
 export default Icon;
