192ea43c8026c3c611908a1a9eea748863ecd963
Katia-2020 <kateflights@gmail.com>
2020-12-23 20:54:17 +0000

    modified sections arr

---
+++ client/app/components/mocks/sections.js
@@ -0,0 +1,29 @@
+const sections = [
+  {
+    id: 0,
+    title: 'What is Mazda?',
+    body:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sequi, commodi magni voluptates molestiae ratione veritatis sunt asperiores debitis reprehenderit, aut adipisci, eos nulla beatae repudiandae voluptas reiciendis nostrum ex! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sequi, commodi magni voluptates molestiae ratione veritatis sunt asperiores debitis reprehenderit, aut adipisci, eos nulla beatae repudiandae voluptas reiciendis nostrum ex!'
+  },
+  {
+    id: 1,
+    title: 'What is BMV?',
+    body:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sequi, commodi magni voluptates molestiae ratione veritatis sunt asperiores debitis reprehenderit, aut adipisci, eos nulla beatae repudiandae voluptas reiciendis nostrum ex!',
+    open: true,
+  },
+  {
+    id: 2,
+    title: 'What is Lada?',
+    body:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sequi, commodi magni voluptates molestiae ratione veritatis sunt asperiores debitis reprehenderit, aut adipisci, eos nulla beatae repudiandae voluptas reiciendis nostrum ex! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sequi, commodi magni voluptates molestiae ratione veritatis sunt asperiores debitis reprehenderit!'
+  },
+  {
+    id: 3,
+    title: 'What is Seat?',
+    body:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sequi, commodi magni voluptates molestiae ratione veritatis sunt asperiores debitis reprehenderit, aut adipisci, eos nulla beatae repudiandae voluptas reiciendis nostrum ex!'
+  }
+];
+
+export default sections;
