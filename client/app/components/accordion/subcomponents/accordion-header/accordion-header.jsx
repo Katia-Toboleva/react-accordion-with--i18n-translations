5eb95df890367a8d96a33bd7d5494f79a46d6cd2
Katia-2020 <kateflights@gmail.com>
2020-12-23 20:52:59 +0000

    accordion-header component

---
+++ client/app/components/accordion/subcomponents/accordion-header/accordion-header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './accordion-header.scss';
+import Text from '../../../text';
+import Icon from '../../../icon';
+
+
+const AccordionHeader = ({ id, title, onClick, isOpened }) => {
+  const handleClick = () => {
+    onClick(id);
+  };
+
+  return (
+    <div className={styles['accordion-header']} onClick={handleClick}>
+      <Text size="medium" weight="medium">{title}</Text>
+      <Icon icon="plus" isOpened={isOpened} size="small" theme="white" />
+    </div>
+  )
+}
+
+export default AccordionHeader;
