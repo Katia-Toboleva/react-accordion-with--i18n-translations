98507222793ef64658b3a08e6531c1e47bdc04fa
Katia-2020 <kateflights@gmail.com>
2020-12-24 15:48:00 +0000

    created languages arr and passed it as props

--- client/app/components/faq/faq.jsx
+++ client/app/components/faq/faq.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import sections from '../mocks/sections';
+import languages from '../mocks/languages'
 import styles from './faq.scss';
 
 import Title from '../title';
+import Buttons from '../buttons';
 import Accordion from '../accordion';
 
 const Faq = () => (
   <div className={styles.faq}>
     <Title />
-    {/* <Buttons /> */}
+    <Buttons languages={languages} />
     <Accordion sections={sections} />
   </div>
 
---
+++ client/app/components/mocks/languages.js
@@ -0,0 +1,16 @@
+const languages = [
+  {
+    id: 0,
+    language: 'en',
+  },
+  {
+    id: 1,
+    language: 'es',
+  },
+  {
+    id: 2,
+    language: 'ru',
+  },
+];
+
+export default languages;
