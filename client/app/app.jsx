9572096586900f20c5ebc036b3bdcad7dc69d841
Katia-2020 <kateflights@gmail.com>
2020-12-24 15:24:15 +0000

    included faq instead of accordion inside app.jsx

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { I18nextProvider } from 'react-i18next';
 
-import Accordion from './components/accordion';
+import Faq from './components/faq';
 
 import './reset.scss';
 import i18n from './translations/config';
 
 const App = () => (
   <I18nextProvider i18n={i18n}>
-    <Accordion />
+    <Faq />
   </I18nextProvider>
 );
 
