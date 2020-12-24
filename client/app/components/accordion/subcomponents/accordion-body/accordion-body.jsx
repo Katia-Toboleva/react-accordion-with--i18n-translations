59c3e3d012c82fa2faf95b113a79386916e0ca62
Katia-2020 <kateflights@gmail.com>
2020-12-24 15:03:59 +0000

    added useRef and useEffect for the accordion to animate height of each accordion height

--- client/app/components/accordion/subcomponents/accordion-body/accordion-body.jsx
+++ client/app/components/accordion/subcomponents/accordion-body/accordion-body.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect, useRef } from 'react';
+import classnames from 'classnames/bind';
 import styles from './accordion-body.scss';
 
-const AccordionBody = ({ isOpened, body }) => (
-  <div className={styles['accordion-body']}>
-    { isOpened && (
-      <div className={styles['accordion-body__content']}>
-        {body}
-      </div>
-    )}
+const cx = classnames.bind(styles);
+
+const getBodyStyle = (height, isOpened) => isOpened ? {height: `${height}px`} : {height: '0px'};
+
+const AccordionBody = ({ isOpened, body }) => {
+  const [height, setHeight] = useState(0);
+  const refBody = useRef();
+
+  useEffect(() => {
+    setHeight(refBody.current.offsetHeight)
+  })
+
+  return (
+  <div className={cx('accordion-body', {
+    'accordion-body--active': isOpened,
+  })}
+    style={getBodyStyle(height, isOpened)}
+  >
+    <div ref={refBody} className={styles['accordion-body__content']}>
+      {body}
+    </div>
   </div>
-)
+)}
 
 export default AccordionBody;
