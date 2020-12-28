0447bf1dacffc2e19bed5c9faef6ed4246ff2d2c
Katia-2020 <kateflights@gmail.com>
2020-12-28 11:40:08 +0000

    handling button on click

--- client/app/components/buttons/buttons.jsx
+++ client/app/components/buttons/buttons.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './buttons.scss';
 import Button from '../button';
 import { Row, Column } from '../grid';
 
-const Buttons = ({ languages }) => (
+const Buttons = ({ languages }) => {
+  const [currentLang, setCurrentLang] = useState('en')
+
+  const handleLanguageButtonClick = (language) => {
+    setCurrentLang(language)
+  }
+
+  return (
   <div className={styles.buttons}>
     <Row gutters="small">
       {languages.map(item => (
         <Column key={item.id} >
-          <Button language={item.language} flag={item.image} />
+          <Button
+            language={item.language}
+            flag={item.image}
+            active={item.language === currentLang}
+            onClick={handleLanguageButtonClick}
+          />
         </Column>
       ))}
     </Row>
   </div>
-);
+)};
 
 export default Buttons;
