01959d6e75f7954f250ede68195ba5d1c270238c
Katia-2020 <kateflights@gmail.com>
2020-12-28 18:08:04 +0000

    included content to the translation files

--- client/app/translations/locales/en-GB.js
+++ client/app/translations/locales/en-GB.js
@@ -1,3 +1,43 @@
 export default {
-  key: 'value',
+  main: {
+    title: 'frequently asked questions',
+    sections: [
+      {
+        id: 0,
+        title: 'Where are you based?',
+        body:
+          'We love our neighbourhood and hope you will too. Holborn is minutes from Covent Garden, the British Museum and Oxford Street, and so much more. We are close to University College London; Central St Martins; Birkbeck, University of London; the University of Westminster and, of course, the London School of Economics.'
+      },
+      {
+        id: 1,
+        title: 'How to book a course?',
+        body:
+          'Our dedicated student services and sales team are on hand every step of the way to ensure you make the most informed decision of the course that best suits your needs and academic or professional goals. The quickest and simplest way to book is online using our website www.bookbookbook.com. Here you will find our current courses and up-to-date promotions.',
+      },
+      {
+        id: 2,
+        title: 'How much does it cost?',
+        body:
+          'You can see our pricelist online: www.prices.com. The price depends on what time you study, how many weeks you study and the type of course.'
+      },
+      {
+        id: 3,
+        title: 'Can you arrange accommodation?',
+        body:
+          'Yes we can arrange homestays, houseshares and various kinds residences. For full details and prices, please look at our student accommodation pages: www.homehome.com or ask at reception.'
+      },
+      {
+        id: 4,
+        title: 'How do I apply for visa?',
+        body:
+          'If you are outside the UK now you can normally apply online. This is the website https://www.gov.uk/check-uk-visa.'
+      },
+      {
+        id: 5,
+        title: 'Do you have WiFi?',
+        body:
+          'Yes.  We have high speed Internet in all our schools. You can link to it wirelessly using your laptop or mobile device.'
+      },
+    ]
+  }
 };
--- client/app/translations/locales/es-ES.js
+++ client/app/translations/locales/es-ES.js
@@ -1,3 +1,43 @@
 export default {
-  key: 'value',
+  main: {
+    title: 'preguntas frequentes',
+    sections: [
+      {
+        id: 0,
+        title: '¿Cuál es vuestra ubicación?',
+        body:
+          'Nos encanta nuestro barrio y esperamos que a ti también. Holborn está unos minutos de Covent Garden, el British Museum, Oxford Street y mucho más. Estamos cerca de la University College de Londres, Central St Martins, Birkbeck, Universidad de Londres, la Universidad de Westminster y, por supuesto, la London School of Economics.'
+      },
+      {
+        id: 1,
+        title: '¿Cómo se puede reservar un curso?',
+        body:
+          'Nuestro dedicado servicio de estudiantes y equipo de ventas, estará pendiente durante el proceso de matriculación para asegurarnos de que tomes la mejor decisión respecto al curso que más encaje a tus necesidades académicas a tus objetivos profesionales. La forma más rápida y sencilla de hacer tu reserva es a través de nuestra página web www.bookbookbook.com. Aquí encontrarás todos nuestros cursos y promociones.',
+      },
+      {
+        id: 2,
+        title: '¿Cuánto cuesta?',
+        body:
+          'Puedes chequear la lista de precios online: www.prices.com. El precio depende del horarion en el que estudias, la cantidad de semanas que resevas y también de tipo de curso.'
+      },
+      {
+        id: 3,
+        title: '¿Podéis reservar mi alojamiento?',
+        body:
+          'Si, podemos organizar estancias en hogares, pisos compartidos y diversos tipos de residencias. Para obtener más detalles y precios, por favor, consulta nuestras páginas sobre alojamiento para estudiantes: www.homehome.com o pregunta en recepción.'
+      },
+      {
+        id: 4,
+        title: '¿Cómo solicito un visado?',
+        body:
+          'Si estás fuera del Reino Unido ahora, normalmente puedes solicitarlo online. Esta es la página web https://www.gov.uk/check-uk-visa.'
+      },
+      {
+        id: 5,
+        title: '¿Hay WiFi en la escuela?',
+        body:
+          'Sí. Tenemos internet de alta velocidad en todos nuestros centros. Puedes acceder a él de forma inalámbrica utilizando tu portátil o tu móvil.'
+      }
+    ]
+  }
 };
