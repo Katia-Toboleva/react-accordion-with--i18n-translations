e72d411e859d76235451bf2192c2ce5bb5b97bce
Katia-2020 <kateflights@gmail.com>
2020-12-28 11:00:32 +0000

    amended structure of the faq

--- client/app/components/faq/faq.jsx
+++ client/app/components/faq/faq.jsx
@@ -3,14 +3,21 @@ import sections from '../mocks/sections';
 import languages from '../mocks/languages'
 import styles from './faq.scss';
 
+import { Row, Column } from '../grid';
 import Title from '../title';
 import Buttons from '../buttons';
 import Accordion from '../accordion';
 
 const Faq = () => (
   <div className={styles.faq}>
-    <Title />
-    <Buttons languages={languages} />
+    <Row>
+      <Column grow>
+        <Title />
+      </Column>
+      <Column shrink>
+        <Buttons languages={languages} />
+      </Column>
+    </Row>
     <Accordion sections={sections} />
   </div>
 
